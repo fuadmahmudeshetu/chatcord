4c5a7797b54385820611587eaa6344d8c36e67fc
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2026-01-02 11:44:17 +0300

    first commit setup server.js and connect to the client

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const { Server } = require('socket.io')
+
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Run the client connects
+io.on('connection', socket => {
+    console.log('New ws connection...')
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`))
