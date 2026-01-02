7a154f64c432d10bdf59d4f167578f5151c45562
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2026-01-02 14:50:19 +0300

    test if the send button is clicked and message send to console

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,11 @@
+const getChatForm = document.getElementById('chat-form');
+
 const socket = io();
 
-socket.on('message', (message) => {
-    console.log(message);
+getChatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const msg = e.target.elements.msg.value;
+
+    console.log(msg);
 })
